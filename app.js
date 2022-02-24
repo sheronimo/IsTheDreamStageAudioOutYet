414bf00169226a52fd8836cd8a9bc5385d6e86e2
Shuber <shuberinu@gmail.com>
2022-02-24 13:49:25 +0700

    fix

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const dateDifference = () => {
+function dateDifference() {
 	const dateDebut = dayjs('2021-12-18');
 	const dateToday = dayjs();
 
 	return dateToday.diff(dateDebut, 'days');
-};
+}
 
 (function updateCount() {
 	const el = document.querySelector('#daycount');
 	const baseCount = parseInt(el.innerText);
+	const datediff = dateDifference();
 
-	if (baseCount < dateDifference()) {
+	if (baseCount < datediff) {
 		el.innerText = baseCount + 1;
 		setTimeout(updateCount, 25);
 	} else {
-		el.innerText = dateDifference();
+		el.innerText = datediff;
 	}
 })();
